2340c7b5944875c945ab7dcf7e72f58ef0bbb64e
fronte-finem <fronte.finem@yandex.ru>
2021-11-08 18:01:03 +0300

    chore: add code style configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-use-before-define': ['error', { functions: false }],
+    'no-underscore-dangle': ['warn', { allowAfterThis: true }],
+    'import/prefer-default-export': 'off',
+  },
+};
