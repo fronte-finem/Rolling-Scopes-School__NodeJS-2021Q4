ad31baf927777b7cdb646fe63202c0439181cb97
fronte-finem <fronte.finem@yandex.ru>
2021-11-10 22:32:32 +0300

    refactor: use error handler

--- ciphering-tool/index.js
+++ ciphering-tool/index.js
@@ -1,7 +1,12 @@
 import { parseArgvToMap } from './utils/parse-argv-to-map.js';
+import { errorHandler } from './utils/error-handler.js';
 
 console.log('ciphering tool');
 console.log(process.argv.slice(2));
 
 console.log();
-console.log(parseArgvToMap(process.argv.slice(2)));
+errorHandler(() => {
+  const map = parseArgvToMap(process.argv.slice(2));
+  process.stdout.write(`${map}\n`);
+  console.log(map);
+});
