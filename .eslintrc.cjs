25343baabe31810fc0473a86016b653e94a4632c
fronte-finem <fronte.finem@yandex.ru>
2021-11-12 21:16:55 +0300

    chore: allow bitwise expressions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
   },
   rules: {
     'no-console': 'off',
+    'no-bitwise': 'off',
     'no-use-before-define': ['error', { functions: false }],
     'no-underscore-dangle': ['warn', { allowAfterThis: true }],
     'import/prefer-default-export': 'off',
