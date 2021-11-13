203960f93bdb910af005708107ab119eb893c98d
fronte-finem <fronte.finem@yandex.ru>
2021-11-13 21:02:43 +0300

    refactor: make enum for CLI options

--- ciphering-tool/configs/cli-options.js
+++ ciphering-tool/configs/cli-options.js
@@ -1,15 +1,24 @@
-export const OptionConfig = Symbol('CLI option: config');
-export const OptionInput = Symbol('CLI option: input');
-export const OptionOutput = Symbol('CLI option: output');
+const getOptionDescription = (name) => `CLI option: ${name}`;
 
-/** @type {Readonly<Map<string, Symbol>>} */
+/**
+ * @type {  Readonly<{ Config: symbol, Input: symbol, Output: symbol}> }
+ */
+export const CLI_OPTION = Object.freeze({
+  Config: Symbol(getOptionDescription('config')),
+  Input: Symbol(getOptionDescription('input')),
+  Output: Symbol(getOptionDescription('output')),
+});
+
+/**
+ * @type { Readonly<Map<string, Symbol>> }
+ */
 export const CLI_OPTIONS_MAP = Object.freeze(
   new Map([
-    ['-c', OptionConfig],
-    ['--config', OptionConfig],
-    ['-i', OptionInput],
-    ['--input', OptionInput],
-    ['-o', OptionOutput],
-    ['--output', OptionOutput],
+    ['-c', CLI_OPTION.Config],
+    ['--config', CLI_OPTION.Config],
+    ['-i', CLI_OPTION.Input],
+    ['--input', CLI_OPTION.Input],
+    ['-o', CLI_OPTION.Output],
+    ['--output', CLI_OPTION.Output],
   ])
 );
