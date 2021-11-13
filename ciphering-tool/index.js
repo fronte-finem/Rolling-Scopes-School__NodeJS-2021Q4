59170f648988feb8e825dc80432d1a79328b7f0f
fronte-finem <fronte.finem@yandex.ru>
2021-11-13 21:05:11 +0300

    refactor: add cipher streams to pipeline

--- ciphering-tool/index.js
+++ ciphering-tool/index.js
@@ -1,21 +1,18 @@
 import { pipeline } from 'stream/promises';
 import { parseArgvToMap } from './utils/parse-argv-to-map.js';
 import { errorHandler } from './utils/error-handler.js';
-import { OptionInput, OptionOutput } from './configs/cli-options.js';
+import { CLI_OPTION } from './configs/cli-options.js';
 import { getInputStream } from './streams/input-stream.js';
 import { getOutputStream } from './streams/output-stream.js';
+import { validateConfig } from './validators/validate-config.js';
 
-console.log('ciphering tool');
-console.log(process.argv.slice(2));
-
-console.log();
 errorHandler(async () => {
-  const map = parseArgvToMap(process.argv.slice(2));
-  process.stdout.write(`${map}\n`);
-  console.log(map);
+  const argvMap = parseArgvToMap(process.argv.slice(2));
+  const cipherStreams = validateConfig(argvMap.get(CLI_OPTION.Config));
 
   await pipeline(
-    getInputStream(map.get(OptionInput)),
-    getOutputStream(map.get(OptionOutput))
+    getInputStream(argvMap.get(CLI_OPTION.Input)),
+    ...cipherStreams.map((SomeCipherStream) => new SomeCipherStream()),
+    getOutputStream(argvMap.get(CLI_OPTION.Output))
   );
 });
