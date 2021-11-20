51b63a8637a9fc09bb33eae6be5fa494ac9c749a
fronte-finem <fronte.finem@yandex.ru>
2021-11-20 22:34:43 +0300

    refactor: extract main and helpers functions

--- ciphering-tool/index.js
+++ ciphering-tool/index.js
@@ -6,21 +6,27 @@ import { InputStream } from './streams/input-stream.js';
 import { OutputStream } from './streams/output-stream.js';
 import { validateConfig } from './validators/validate-config.js';
 
-errorHandler(async () => {
+const getInputStream = (inputPath) =>
+  inputPath ? new InputStream(inputPath) : process.stdin;
+
+const getOutputStream = (outputPath) =>
+  outputPath ? new OutputStream(outputPath) : process.stdout;
+
+const main = async () => {
   const argvMap = parseArgvToMap(process.argv.slice(2));
   const cipherStreamClasses = validateConfig(argvMap.get(CLI_OPTION.CONFIG));
 
   const inputPath = argvMap.get(CLI_OPTION.INPUT);
   const outputPath = argvMap.get(CLI_OPTION.OUTPUT);
 
-  const inputStream = inputPath ? new InputStream(inputPath) : process.stdin;
-  const outputStream = outputPath
-    ? new OutputStream(outputPath)
-    : process.stdout;
+  const inputStream = getInputStream(inputPath);
+  const outputStream = getOutputStream(outputPath);
 
   const cipherStreams = cipherStreamClasses.map(
     (SomeCipherStreamClass) => new SomeCipherStreamClass()
   );
 
   await pipeline(inputStream, ...cipherStreams, outputStream);
-});
+};
+
+await errorHandler(main);
